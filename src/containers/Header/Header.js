48240d29c7f2f531571791f0a86a6f9bd25cccc6
Fuc le <quangphuchoctapit@gmail.com>
2023-08-29 18:16:05 +0700

    finish menu authorisation for admin -react-redux

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -6,15 +6,15 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils/constant'
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
     handleChangeLanguage = (language) => {
         this.props.changeLanguageAppRedux(language)
 
     }
-
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
         return (
             <div className="header-container">
                 {/* thanh navigator */}
@@ -22,6 +22,7 @@ class Header extends Component {
                     <Navigator menus={adminMenu} />
                 </div>
                 <div className='languages'>
+                    <span className='welcome'>< FormattedMessage id="homeheader.welcome" /> {userInfo && userInfo.firstName ? userInfo.firstName : ''}</span>
                     <span className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
                         onClick={() => { this.handleChangeLanguage(LANGUAGES.VI) }}
                     >
@@ -48,6 +49,7 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
         language: state.app.language
 
     };
